da9f1531ff663a153ce25d1ac43c9d40694d4969
Bruno Sánchez <90653850+BrunoSaPa@users.noreply.github.com>
2026-01-27 14:16:39 -0600

    Add Session03_20260127 Java project with image generation
    Added a new Java project 'Session03_20260127' with IntelliJ IDEA configuration files, .gitignore, and source code. The main functionality generates an image with colored triangles and saves it as 'image.jpg'.

---
+++ Session03_20260127/src/edu/up/cg/images/Main.java
@@ -0,0 +1,36 @@
+package edu.up.cg.images;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args){
+        int scalar = 4;
+        int height = 300*scalar;
+        int width = 400*scalar;
+        BufferedImage image = new BufferedImage(width,height,BufferedImage.TYPE_3BYTE_BGR);
+
+        //lower half
+        for(int y=0 ; y < height; y++){
+            for(int x=0; x<width; x++){
+                boolean isLowerTriangle =  y >=  (double)height / width * x;
+                if(isLowerTriangle){
+                    image.setRGB(x,y, Color.blue.getRGB());
+                }else{
+                    image.setRGB(x,y, Color.red.getRGB());
+                }
+            }
+        }
+
+
+        File outputImage = new File("image.jpg");
+        try{
+            ImageIO.write(image,"jpg", outputImage);
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
